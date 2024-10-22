6acdf31981b32722794994c7771cf99650e991db
ILKABU <42551097+ILKABU@users.noreply.github.com>
2024-10-22 15:19:33 +0300

    Create config_flow.py

---
+++ custom_components/htx_api/config_flow.py
@@ -0,0 +1,29 @@
+"""Config flow for HTX API integration."""
+from __future__ import annotations
+
+from typing import Any
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.core import HomeAssistant
+from homeassistant.data_entry_flow import FlowResult
+from homeassistant.exceptions import HomeAssistantError
+
+from .const import DOMAIN
+
+class HTXAPIConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for HTX API."""
+
+    VERSION = 1
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the initial step."""
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+
+        if user_input is None:
+            return self.async_show_form(step_id="user")
+
+        return self.async_create_entry(title="HTX API", data={})
