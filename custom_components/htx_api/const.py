49bff639308ee1ec957afec702cc1e7d3ee72446
ILKABU <42551097+ILKABU@users.noreply.github.com>
2024-10-22 14:44:48 +0300

    Create const.py

---
+++ custom_components/htx_api/const.py
@@ -0,0 +1,19 @@
+"""Constants for HTX API integration."""
+from datetime import timedelta
+
+DOMAIN = "htx_api"
+SCAN_INTERVAL = timedelta(minutes=1)
+
+PAYMENT_METHODS = {
+    "28": "Тинькофф",
+    "29": "Сбербанк",
+    "36": "Райффайзенбанк",
+    "69": "СБП",
+    "70": "Переводы в определенный банк",
+    "75": "СБП",
+    "45": "ОТП Банк",
+    "351": "Газпромбанк"
+}
+
+TARGET_PAYMENT_METHODS = ["Сбербанк", "Тинькофф", "Райффайзенбанк", "СБП"]
+BASE_URL = "https://otc-api.trygofast.com/v1/data/trade-market"
