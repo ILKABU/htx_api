f122284b4914c92f945950e034094078b4fc3d73
ILKABU <42551097+ILKABU@users.noreply.github.com>
2024-10-22 16:08:38 +0300

    Update config_flow.py

--- custom_components/htx_api/config_flow.py
+++ custom_components/htx_api/config_flow.py
@@ -7,9 +7,9 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
-from homeassistant.exceptions import HomeAssistantError
+from homeassistant.helpers import config_validation as cv
 
-from .const import DOMAIN
+from .const import DOMAIN, CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
 
 class HTXAPIConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for HTX API."""
@@ -24,6 +24,19 @@ class HTXAPIConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_abort(reason="single_instance_allowed")
 
         if user_input is None:
-            return self.async_show_form(step_id="user")
+            return self.async_show_form(
+                step_id="user",
+                data_schema=vol.Schema({
+                    vol.Optional(
+                        CONF_SCAN_INTERVAL,
+                        default=DEFAULT_SCAN_INTERVAL.total_seconds()
+                    ): vol.All(vol.Coerce(int), vol.Range(min=1)),
+                })
+            )
 
-        return self.async_create_entry(title="HTX API", data={})
+        return self.async_create_entry(
+            title="HTX API",
+            data={
+                CONF_SCAN_INTERVAL: user_input[CONF_SCAN_INTERVAL],
+            },
+        )
